69b61bc5da5250cb7941ab41a365f69cbc483cad
rejohnbd <rejohn.cse@gmail.com>
2022-10-20 02:34:27 +0600

    Navigate to Dynamic Routes

--- next-co/pages/clients/index.js
+++ next-co/pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 function ClientPage() {
+  const clients = [
+    { id: "rahat", name: "Rahat" },
+    { id: "rejohn", name: "Rejohn" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

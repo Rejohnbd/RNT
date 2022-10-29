1e32f5e906fc5c500290fdc4ca827e994db773b5
rejohnbd <rejohn.cse@gmail.com>
2022-10-30 02:01:18 +0600

    Use of fallback option at getSTaticPaths method

--- next-pre-render/pages/[pid].js
+++ next-pre-render/pages/[pid].js
@@ -4,6 +4,10 @@ import { Fragment } from "react";
 
 function ProductDetilPage(props) {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -32,11 +36,13 @@ export async function getStaticProps(context) {
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { pid: "p1" } },
-      { params: { pid: "p2" } },
-      { params: { pid: "p3" } },
+      // { params: { pid: "p1" } },
+      // { params: { pid: "p2" } },
+      // { params: { pid: "p3" } },
     ],
-    fallback: false,
+    // fallback key work hee for a lot ot page that would need to be pre-generated.
+    fallback: true,
+    // fallback: 'blocking,
   };
 }
 

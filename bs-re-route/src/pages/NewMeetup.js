13e5d03837cfccec12e18d8a30db7daa9753a943
rejohnbd <rejohn.cse@gmail.com>
2022-11-06 02:13:30 +0600

    Working with Forms

--- bs-re-route/src/pages/NewMeetup.js
+++ bs-re-route/src/pages/NewMeetup.js
@@ -1,4 +1,11 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-  return <div>New Meetup</div>;
+  return (
+    <div>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </div>
+  );
 }
 export default NewMeetupPage;

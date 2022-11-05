bf36bb36c8f72f20d1c27fd1f3cdce41a535b477
rejohnbd <rejohn.cse@gmail.com>
2022-11-06 00:41:02 +0600

    Adding Layout Component and Wrapped Component

--- bs-re-route/src/App.js
+++ bs-re-route/src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+import Layout from "./components/layout/Layout";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <MainNavigation />
-      <Routes>
-        <Route path="/" element={<AllMeetupsPage />} />
-        <Route path="/new-meetup" element={<NewMeetupPage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AllMeetupsPage />} />
+          <Route path="/new-meetup" element={<NewMeetupPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }

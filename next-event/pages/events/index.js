cda89aa53760b897984b391db31f6283f51903ac
rejohnbd <rejohn.cse@gmail.com>
2022-11-15 02:24:48 +0600

    fetch all data using getStaticProps

--- next-event/pages/events/index.js
+++ next-event/pages/events/index.js
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/event-search";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 
-function AllEventPage() {
+function AllEventPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}/abc`;
@@ -18,6 +18,14 @@ function AllEventPage() {
       <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: { events },
+    revalidate: 60,
+  };
 }
 
 export default AllEventPage;

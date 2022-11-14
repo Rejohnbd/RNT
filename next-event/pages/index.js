4594c06499e4d1a56c710192a660c6e0a49a8b14
rejohnbd <rejohn.cse@gmail.com>
2022-11-15 01:55:34 +0600

    Loading Data & Path for Dynamic pages

--- next-event/pages/index.js
+++ next-event/pages/index.js
@@ -11,7 +11,6 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   const featuredEvents = await getFeaturedEvents();
-  console.log(featuredEvents);
   return {
     props: {
       events: featuredEvents,
